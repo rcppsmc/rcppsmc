908960e8e1d997985f8cc535ba96b1cd3c0b6526
Adam Johansen <adam.johansen@gmail.com>
2012-03-18 19:52:08 +0000

    Standardised plotting output.

--- R/bspfGaussianOptimal.R
+++ R/bspfGaussianOptimal.R
@@ -1,4 +1,3 @@
-
 blockpfGaussianOpt <- function(data=c(), particles=1000, lag=5, plot=FALSE) {
 
     if (length(data == 0)) {
@@ -13,15 +12,14 @@ blockpfGaussianOpt <- function(data=c(), particles=1000, lag=5, plot=FALSE) {
 	sqvect = t(res$weight) %*% res$values^2 / sum(res$weight);
 	sdvect = sqrt(sqvect - mvect^2);
  
-       plot(time, mvect, col='dark red', 'l', lty = 1, lwd=3, xlab = 'Iteration',
-       ylab='State', main='Mean and 1, 2 standard deviation credible
-       intervals with observations', xlim = c(0,length(data)), ylim=c(min(mvect - 2.1
-       * (sdvect)), max(mvect+2.1*sdvect))
-       )
-       lines(time, mvect + sdvect, lty=3, col='dark blue')
-       lines(time, mvect - sdvect, lty=3, col='dark blue')
-       lines(time, mvect + 2 * sdvect, lty=2, col='dark blue')
-       lines(time, mvect - 2 * sdvect, lty=2, col='dark blue')
+       plot(time, mvect, 'l', lty = 1, lwd=3, xlab = 'Iteration', ylab='State', 
+            main='Mean and 1, 2 standard deviation credible intervals with observations', 
+	    xlim = c(0,length(data)), ylim=c(min(mvect - 2.1 * (sdvect)), max(mvect+2.1*sdvect)))
+
+       polygon(c(time,seq(length(data),1,-1)),c(mvect-2*sdvect,(mvect+2*sdvect)[seq(length(data),1,-1)]),col='palegreen1',border=NA)
+       polygon(c(time,seq(length(data),1,-1)),c(mvect-1*sdvect,(mvect+1*sdvect)[seq(length(data),1,-1)]),col='palegreen3',border=NA)
+       lines(time, mvect, lwd=2, col='dark blue')
+
        points(time, data, col = 'dark green', cex=0.5)
     }
 
--- R/pfNonlinBS.R
+++ R/pfNonlinBS.R
@@ -7,7 +7,7 @@ pfNonlinBS <- function(data=c(), particles=500, plot=FALSE) {
 
     time <- 1:length(data);
     if (plot) {
-      with(res, plot(time,mean,type='l',xlab='time',ylab='estimate'))
+      with(res, plot(time,mean,type='l',xlab='time',ylab='estimate',xlim = c(0,length(data)), ylim = c(min(mean-2.1*sd),max(mean+2.1*sd))))
       with(res,polygon(c(time,seq(length(data),1,-1)),c(mean-2*sd,(mean+2*sd)[seq(length(data),1,-1)]),col='palegreen1',border=NA))
       with(res,polygon(c(time,seq(length(data),1,-1)),c(mean-1*sd,(mean+1*sd)[seq(length(data),1,-1)]),col='palegreen3',border=NA))
       with(res,lines(time,mean, lwd=2, col='darkblue'))
