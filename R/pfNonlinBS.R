d0998ebfedf5a4f2ccd3fccfc0b0d38d6f8a45de
Adam Johansen <adam.johansen@gmail.com>
2012-03-18 11:11:44 +0000

    Fixing indexing bug in plotting routine.

--- R/pfNonlinBS.R
+++ R/pfNonlinBS.R
@@ -8,8 +8,8 @@ pfNonlinBS <- function(data=c(), particles=500, plot=FALSE) {
     time <- 1:length(data);
     if (plot) {
       with(res, plot(time,mean,type='l',xlab='time',ylab='estimate'))
-      with(res,polygon(c(time,51-time),c(mean-2*sd,(mean+2*sd)[seq(length(data),1,-1)]),col='palegreen1',border=NA))
-      with(res,polygon(c(time,51-time),c(mean-1*sd,(mean+1*sd)[seq(length(data),1,-1)]),col='palegreen3',border=NA))
+      with(res,polygon(c(time,seq(length(data),1,-1)),c(mean-2*sd,(mean+2*sd)[seq(length(data),1,-1)]),col='palegreen1',border=NA))
+      with(res,polygon(c(time,seq(length(data),1,-1)),c(mean-1*sd,(mean+1*sd)[seq(length(data),1,-1)]),col='palegreen3',border=NA))
       with(res,lines(time,mean, lwd=2, col='darkblue'))
     }
 
