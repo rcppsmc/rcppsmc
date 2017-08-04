b85c98d88641638d8685d1137d043999dd28bd26
Leah South <leah.south@hdr.qut.edu.au>
2017-08-04 10:45:48 +1000

    Fixing Data NOTE
    Was previously loading data(radiata) into the global workspace. Added envir = environment() to silence note from R CMD check.

--- R/LinRegLA.R
+++ R/LinRegLA.R
@@ -1,6 +1,6 @@
 LinRegLA<- function(model, particles=1000,temperatures = seq(0,1,0.05)^5) {
 
-    data(radiata)
+    data(radiata, envir = environment())
     if (model ==1){
         Data <- cbind(radiata$y,radiata$x1)
     } else if (model == 2){
