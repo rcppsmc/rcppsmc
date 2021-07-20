e631e515afbb9aa01792d2392c35620790db242c
Ilya Zarubin <zarubin@wiso.uni-koeln.de>
2021-07-20 16:57:42 +0200

    additions to conditional SMC
    - add specific Initialize functions
    - change the order of conditioning: now, conditioning is performed exactly after
    initialization or propagation
    - TO-DO (IMPORTANT):
      * conditioning coordinate needs to be re-weighted and the weights
      need to be re-normalized
      * get rid of, or adjust, facilities for adaptation; adaptive resampling; additional MCMC moves and similar

--- inst/include/sampler.h
+++ inst/include/sampler.h
@@ -980,7 +980,10 @@ namespace smc {
             void SetReferenceTrajectory(const std::vector<Space>& newReferenceTrajectory) {referenceTrajectory = newReferenceTrajectory;}
             ///Sets the n'elment of the reference trajectory
             void SetReferenceValue(const Space & newReferenceValue, long n) {referenceTrajectory[n] = newReferenceValue;}
-
+            ///Initialise the conditional sampler and its constituent particles. The reference trajectory used to set the first coordinate at T=0 is either from the last call to conditionalSampler.SetReferenceTrajectory(), or defaults to the trajectory passed to the constructor at the time the object is created.
+            void Initialise(void);
+            ///Initialise the sampler and its constituent particles.
+            void Initialise(const std::vector<Space>& newReferenceTrajectory);
             ///Perform one conditional iteration of the simulation algorithm.
             void Iterate(void);
             ///Perform one conditional iteration of the simulation algorithm and return the resulting ess
@@ -989,14 +992,167 @@ namespace smc {
             void IterateUntil(long lTerminate);
 
     };
+    /// At present this function resets the system evolution time to 0 and calls the moveset initialisor to assign each particle in the ensemble.
+    ///
+    /// Note that the initialisation function must be specified before calling this function.
+    ///
+    /// Additionally, the t=0 particle coordinate is set to the conditional reference value and re-weighted.
     template <class Space, class Params>
-    double conditionalSampler<Space,Params>::IterateEss(void)
+    void conditionalSampler<Space,Params>::Initialise(void)
+    {
+        sampler<Space,Params>::T = 0;
+        sampler<Space,Params>::dlogNCPath = 0.0;
+        sampler<Space,Params>::acceptProb = -1;
+
+        //Set the initial values and log weights of the particles
+        std::vector<Space> InitVal(sampler<Space,Params>::N);
+        arma::vec InitWeights(sampler<Space,Params>::N);
+        sampler<Space,Params>::pPopulation = population<Space>(InitVal,InitWeights);
+        sampler<Space,Params>::pMoves->DoInit(sampler<Space,Params>::pPopulation,sampler<Space,Params>::N,sampler<Space,Params>::algParams);
+        //Set conditonal particle coordinate at iteration T=0
+        sampler<Space,Params>::pPopulation.SetValueN(GetReferenceValueRefs(0), 0);
+        //Scaling weights by 1/N (for evidence computation)
+        sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - log(static_cast<double>(sampler<Space,Params>::N)));
+
+        //Estimate the normalising constant
+        sampler<Space,Params>::dlogNCIt = sampler<Space,Params>::CalcLogNC();
+        sampler<Space,Params>::dlogNCPath += sampler<Space,Params>::dlogNCIt;
+
+        //Normalise the weights
+        sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - sampler<Space,Params>::dlogNCIt);
+
+        //Check if the ESS is below some reasonable threshold and resample if necessary.
+        //A mechanism for setting this threshold is required.
+        double ESS = sampler<Space,Params>::GetESS();
+        if(ESS < sampler<Space,Params>::dResampleThreshold) {
+            sampler<Space,Params>::nResampled = 1;
+            sampler<Space,Params>::pAdapt->updateForMCMC(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation,sampler<Space,Params>::acceptProb,sampler<Space,Params>::nResampled,sampler<Space,Params>::nRepeats);
+            sampler<Space,Params>::Resample(sampler<Space,Params>::rtResampleMode);
+        }
+        else {
+            sampler<Space,Params>::nResampled = 0;
+            if(HistoryType::AL) {
+                sampler<Space,Params>::uRSIndices = arma::linspace<arma::Col<unsigned int>>(0, sampler<Space,Params>::N - 1, sampler<Space,Params>::N);
+            }
+            sampler<Space,Params>::pAdapt->updateForMCMC(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation,sampler<Space,Params>::acceptProb,sampler<Space,Params>::nResampled,sampler<Space,Params>::nRepeats);
+        }
+
+        //A possible MCMC step should be included here.
+        bool didMCMC =  sampler<Space,Params>::pMoves->DoMCMC(0,sampler<Space,Params>::pPopulation, sampler<Space,Params>::N, sampler<Space,Params>::nRepeats, sampler<Space,Params>::nAccepted, sampler<Space,Params>::algParams);
+        if (didMCMC){
+            sampler<Space,Params>::acceptProb = static_cast<double>(sampler<Space,Params>::nAccepted)/(static_cast<double>(sampler<Space,Params>::N)*static_cast<double>(sampler<Space,Params>::nRepeats));
+        }
+        //Normalise the weights
+        sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - sampler<Space,Params>::CalcLogNC());
+
+        //Perform any final updates to the additional algorithm parameters.
+        sampler<Space,Params>::pAdapt->updateEnd(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation);
+
+        //Finally, the current particle set should be appended to the historical process.
+        if(sampler<Space,Params>::htHistoryMode != HistoryType::NONE){
+            sampler<Space,Params>::History.clear();
+            historyelement<Space> histel;
+            switch(sampler<Space,Params>::htHistoryMode) {
+            case HistoryType::RAM:
+                histel.Set(sampler<Space,Params>::N, sampler<Space,Params>::pPopulation, sampler<Space,Params>::nAccepted, sampler<Space,Params>::nRepeats, historyflags(sampler<Space,Params>::nResampled));
+                break;
+            case HistoryType::AL:
+                histel.Set(sampler<Space,Params>::N, sampler<Space,Params>::pPopulation, sampler<Space,Params>::nAccepted, sampler<Space,Params>::nRepeats, historyflags(sampler<Space,Params>::nResampled), sampler<Space,Params>::uRSIndices);
+                break;
+            /// To avoid compiler warnings, HistoryType::NONE is handled
+            case HistoryType::NONE:
+                break;
+            }
+            sampler<Space,Params>::History.push_back(histel);
+        }
+        return;
+    }
+    /// At present this function resets the system evolution time to 0 and calls the moveset initialisor to assign each particle in the ensemble.
+    ///
+    /// Note that the initialisation function must be specified before calling this function.
+    ///
+    /// Additionally, a conditional particle trajectory is set and the t=0 particle coordinate is set to the conditional reference value and re-weighted.
+    template <class Space, class Params>
+    void conditionalSampler<Space,Params>::Initialise(const std::vector<Space> & referenceTrajectoryInit)
+    {
+        SetReferenceTrajectory(referenceTrajectoryInit);
+        sampler<Space,Params>::T = 0;
+        sampler<Space,Params>::dlogNCPath = 0.0;
+        sampler<Space,Params>::acceptProb = -1;
+
+        //Set the initial values and log weights of the particles
+        std::vector<Space> InitVal(sampler<Space,Params>::N);
+        arma::vec InitWeights(sampler<Space,Params>::N);
+        sampler<Space,Params>::pPopulation = population<Space>(InitVal,InitWeights);
+        sampler<Space,Params>::pMoves->DoInit(sampler<Space,Params>::pPopulation,sampler<Space,Params>::N,sampler<Space,Params>::algParams);
+        //Set conditonal particle coordinate at iteration T=0
+        sampler<Space,Params>::pPopulation.SetValueN(GetReferenceValueRefs(0), 0);
+        //Scaling weights by 1/N (for evidence computation)
+        sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - log(static_cast<double>(sampler<Space,Params>::N)));
+
+        //Estimate the normalising constant
+        sampler<Space,Params>::dlogNCIt = sampler<Space,Params>::CalcLogNC();
+        sampler<Space,Params>::dlogNCPath += sampler<Space,Params>::dlogNCIt;
+
+        //Normalise the weights
+        sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - sampler<Space,Params>::dlogNCIt);
+
+        //Check if the ESS is below some reasonable threshold and resample if necessary.
+        //A mechanism for setting this threshold is required.
+        double ESS = sampler<Space,Params>::GetESS();
+        if(ESS < sampler<Space,Params>::dResampleThreshold) {
+            sampler<Space,Params>::nResampled = 1;
+            sampler<Space,Params>::pAdapt->updateForMCMC(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation,sampler<Space,Params>::acceptProb,sampler<Space,Params>::nResampled,sampler<Space,Params>::nRepeats);
+            sampler<Space,Params>::Resample(sampler<Space,Params>::rtResampleMode);
+        }
+        else {
+            sampler<Space,Params>::nResampled = 0;
+            if(HistoryType::AL) {
+                sampler<Space,Params>::uRSIndices = arma::linspace<arma::Col<unsigned int>>(0, sampler<Space,Params>::N - 1, sampler<Space,Params>::N);
+            }
+            sampler<Space,Params>::pAdapt->updateForMCMC(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation,sampler<Space,Params>::acceptProb,sampler<Space,Params>::nResampled,sampler<Space,Params>::nRepeats);
+        }
+
+        //A possible MCMC step should be included here.
+        bool didMCMC =  sampler<Space,Params>::pMoves->DoMCMC(0,sampler<Space,Params>::pPopulation, sampler<Space,Params>::N, sampler<Space,Params>::nRepeats, sampler<Space,Params>::nAccepted, sampler<Space,Params>::algParams);
+        if (didMCMC){
+            sampler<Space,Params>::acceptProb = static_cast<double>(sampler<Space,Params>::nAccepted)/(static_cast<double>(sampler<Space,Params>::N)*static_cast<double>(sampler<Space,Params>::nRepeats));
+        }
+        //Normalise the weights
+        sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - sampler<Space,Params>::CalcLogNC());
+
+        //Perform any final updates to the additional algorithm parameters.
+        sampler<Space,Params>::pAdapt->updateEnd(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation);
+
+        //Finally, the current particle set should be appended to the historical process.
+        if(sampler<Space,Params>::htHistoryMode != HistoryType::NONE){
+            sampler<Space,Params>::History.clear();
+            historyelement<Space> histel;
+            switch(sampler<Space,Params>::htHistoryMode) {
+            case HistoryType::RAM:
+                histel.Set(sampler<Space,Params>::N, sampler<Space,Params>::pPopulation, sampler<Space,Params>::nAccepted, sampler<Space,Params>::nRepeats, historyflags(sampler<Space,Params>::nResampled));
+                break;
+            case HistoryType::AL:
+                histel.Set(sampler<Space,Params>::N, sampler<Space,Params>::pPopulation, sampler<Space,Params>::nAccepted, sampler<Space,Params>::nRepeats, historyflags(sampler<Space,Params>::nResampled), sampler<Space,Params>::uRSIndices);
+                break;
+            /// To avoid compiler warnings, HistoryType::NONE is handled
+            case HistoryType::NONE:
+                break;
+            }
+            sampler<Space,Params>::History.push_back(histel);
+        }
+        return;
+    }
+    template <class Space, class Params>
+    double conditionalSampler<Space,Params>::IterateEss()
     {
 
         sampler<Space,Params>::pAdapt->updateForMove(this->algParams,sampler<Space,Params>::pPopulation);
 
         //Move the particle set.
         MoveParticles();
+        //Set conditonal particle coordinate at iteration T
+        sampler<Space,Params>::pPopulation.SetValueN(GetReferenceValueRefs(sampler<Space,Params>::T), 0);
 
         // //Estimate the normalising constant
         sampler<Space,Params>::dlogNCIt = sampler<Space,Params>::CalcLogNC();
@@ -1027,10 +1183,9 @@ namespace smc {
             sampler<Space,Params>::acceptProb = static_cast<double>(sampler<Space,Params>::nAccepted)/(static_cast<double>(sampler<Space,Params>::N)*static_cast<double>(sampler<Space,Params>::nRepeats));
         }
 
+
         //Normalise the weights
         sampler<Space,Params>::pPopulation.SetLogWeight(sampler<Space,Params>::pPopulation.GetLogWeight() - sampler<Space,Params>::CalcLogNC());
-        //Set conditonal particle values
-        sampler<Space,Params>::pPopulation.SetValueN(GetReferenceValueRefs(0), 0);
 
         //Perform any final updates to the additional algorithm parameters.
         sampler<Space,Params>::pAdapt->updateEnd(sampler<Space,Params>::algParams,sampler<Space,Params>::pPopulation);
